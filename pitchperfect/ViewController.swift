f2524d2cc7e0080e921f47fc7a92ecfba8c5ccb2
John David Stroud <davidstroud@MacBook-Pro.attlocal.net>
2015-06-26 14:06:19 -0500

    Initial Commit

---
+++ pitchperfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pitchperfect
+//
+//  Created by John David Stroud on 6/26/15.
+//  Copyright (c) 2015 John David Stroud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
