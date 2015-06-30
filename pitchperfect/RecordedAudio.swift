f54be4723464c0c598a17e61489de281bfb068ca
David Stroud <david@greymatter-ag.com>
2015-06-29 20:43:05 -0500

    first commit

---
+++ pitchperfect/RecordedAudio.swift
@@ -0,0 +1,9 @@
+//
+//  RecordedAudio.swift
+//  pitchperfect
+//
+//  Created by John David Stroud on 6/27/15.
+//  Copyright (c) 2015 John David Stroud. All rights reserved.
+//
+
+import Foundation
